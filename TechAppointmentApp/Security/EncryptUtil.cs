ca02c5bef278bcc32674f9953b8ba10ecc374da8
Dimi Serafeimidis <d.serafeimidis@outlook.com>
2025-01-29 15:55:05 +0200

    29/01/25 Models: checked. Security: checked.

--- TechAppointmentApp/Security/EncryptUtil.cs
+++ TechAppointmentApp/Security/EncryptUtil.cs
@@ -3,20 +3,15 @@ namespace TechAppointmentApp.Security
 {
     public static class EncryptionUtil
     {
-        public static string Encrypt(string plaintText)
+        public static string Encrypt(string plainText)
         {
-            var encryptedPassword = BCrypt.Net.BCrypt.HashPassword(plaintText);
-            return encryptedPassword;
+            return BCrypt.Net.BCrypt.HashPassword(plainText);
+             
         }
 
-        public static bool IsValidPassword(string plaintText, string cipherText)
+        public static bool IsValidPassword(string plainText, string cipherText)
         {
-            return BCrypt.Net.BCrypt.Verify(plaintText, cipherText);
-        }
-
-        internal static bool IsValidPassword(string password1, object password2)
-        {
-            throw new NotImplementedException();
+            return BCrypt.Net.BCrypt.Verify(plainText, cipherText);
         }
     }
 }
